cfdd7f5488a847bf671b69b5375d439fc662ece7
Snimkarde <sagar.nim23@gmail.com>
2018-08-11 12:27:47 +0530

    First Commit

---
+++ HotelManagementSystem.Tests/AddHotelSteps.cs
@@ -0,0 +1,53 @@
+﻿using FluentAssertions;
+using HotelManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+
+namespace HotelManagementSystem.Tests
+{
+    [Binding]
+    public class AddHotelSteps
+    {
+        private Hotel hotel = new Hotel();
+        private Hotel addHotelResponse;
+        private List<Hotel> hotels = new List<Hotel>();
+
+
+        [Given(@"User provided valid Id '(.*)'  and '(.*)'for hotel")]
+        public void GivenUserProvidedValidIdAndForHotel(int id, string name)
+        {
+            hotel.Id = id;
+            hotel.Name = name;
+        }
+
+        [Given(@"Use has added required details for hotel")]
+        public void GivenUseHasAddedRequiredDetailsForHotel()
+        {
+            SetHotelBasicDetails();
+        }
+
+        [When(@"User calls AddHotel api")]
+        public void WhenUserCallsAddHotelApi()
+        {
+            hotels = HotelsApiCaller.AddHotel(hotel);
+        }
+
+        [Then(@"Hotel with name '(.*)' should be present in the response")]
+        public void ThenHotelWithNameShouldBePresentInTheResponse(string name)
+        {
+            hotel = hotels.Find(htl => htl.Name.ToLower().Equals(name.ToLower()));
+            hotel.Should().NotBeNull(string.Format("Hotel with name {0} not found in response",name));
+        }
+
+
+        private void SetHotelBasicDetails()
+        {
+            hotel.ImageURLs = new List<string>() { "image1", "image2" };
+            hotel.LocationCode = "Location";
+            hotel.Rooms = new List<Room>() { new Room() { NoOfRoomsAvailable = 10, RoomType = "delux" } };
+            hotel.Address = "Address1";
+            hotel.Amenities = new List<string>() { "swimming pool", "gymnasium" };
+        }
+    }
+}
---
+++ HotelManagementSystem.Tests/HotelsApiCaller.cs
@@ -0,0 +1,24 @@
+﻿using HotelManagementSystem.Models;
+using Newtonsoft.Json;
+using RestSharp;
+using System.Collections.Generic;
+
+
+
+namespace HotelManagementSystem.Tests
+{
+    public static class HotelsApiCaller
+    {
+        private static RestClient client = new RestClient("http://localhost:52475/api");
+        public static List<Hotel> AddHotel(Hotel hotel)
+        {
+            var request = new RestRequest("Hotel", Method.POST);
+            string json = JsonConvert.SerializeObject(hotel);
+            request.AddParameter("application/json", json, ParameterType.RequestBody);
+
+            IRestResponse response = client.Execute(request);
+            var content = response.Content;
+            return JsonConvert.DeserializeObject<List<Hotel>>(content);
+        }
+    }
+}
